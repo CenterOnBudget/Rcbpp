9a7437f00c8f5d5c24d65f8b5b7592ad117f5e28
Matt Saenz <55054385+matt-saenz@users.noreply.github.com>
2021-09-10 10:57:52 -0400

    Add CPI-U-RS data (#7)

--- R/data.R
+++ R/data.R
@@ -8,3 +8,12 @@
 #' @format A \href{https://tibble.tidyverse.org/}{tibble} with three variables:
 #'   \code{state_fips}, \code{state_abbrv}, and \code{state_name}.
 "state_fips"
+
+
+#' CPI-U-RS
+#'
+#' A dataset containing all available CPI-U-RS calendar year averages.
+#'
+#' @format A \href{https://tibble.tidyverse.org/}{tibble} with two variables:
+#'   \code{year} and \code{cpi_u_rs}.
+"cpi_u_rs"
