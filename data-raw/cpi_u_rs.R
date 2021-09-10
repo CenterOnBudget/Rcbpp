875695c8e12f617d225a315714068060c757cc23
matt-saenz <55054385+matt-saenz@users.noreply.github.com>
2021-09-10 09:44:00 -0400

    Tweak

--- data-raw/cpi_u_rs.R
+++ data-raw/cpi_u_rs.R
@@ -16,6 +16,6 @@ raw_cpi_u_rs <- read_xlsx("data-raw/cpi_u_rs.xlsx", skip = 5)
 cpi_u_rs <- raw_cpi_u_rs %>%
   rename_with(tolower) %>%
   select(year, cpi_u_rs = avg) %>%
-  filter(year > 1977)
+  filter(year >= 1978)
 
 usethis::use_data(cpi_u_rs, overwrite = TRUE)
