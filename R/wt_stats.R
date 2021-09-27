0cfe757eba55ac35de68a9cb025e6621b2426e95
Matt Saenz <55054385+matt-saenz@users.noreply.github.com>
2021-09-27 17:26:16 -0400

    Add weighted quantile function (#11)

--- R/wt_stats.R
+++ R/wt_stats.R
@@ -8,7 +8,18 @@
 #'
 #' @param x A logical or numeric vector.
 #' @param wt A numeric vector of weights the same length as \code{x}.
-#' @return A numeric vector of length one.
+#' @param n A numeric vector of length one (i.e., a number) giving the number
+#'   of equally sized groups to split \code{x} into. Note that for \code{n}
+#'   groups, there are \code{n - 1} quantiles (i.e., cut points). Quantiles
+#'   currently supported include the median (\code{n = 2}), quartiles
+#'   (\code{n = 4}), quintiles (\code{n = 5}), deciles (\code{n = 10}), and
+#'   ventiles (\code{n = 20}).
+#' @return For all except \code{wt_quantile()}, a numeric vector of length
+#'   one.
+#'
+#'   For \code{wt_quantile()}, a named numeric vector of length \code{n - 1}
+#'   unless any \code{NA} values are present in \code{x} or \code{wt}, in which
+#'   case a single \code{NA} is returned.
 #' @name wt_stats
 NULL
 
@@ -32,14 +43,35 @@ wt_mean <- function(x, wt) {
 # References:
 # https://en.wikipedia.org/wiki/Weighted_median
 # https://www.stata.com/manuals/dpctile.pdf (page 11)
+# https://www.stata.com/manuals/rsummarize.pdf (pages 9-10)
+
 #' @rdname wt_stats
 #' @export
 wt_median <- function(x, wt) {
+  unname(wt_quantile(x, wt, n = 2))
+}
+
+#' @rdname wt_stats
+#' @export
+wt_quantile <- function(x, wt, n) {
+
+  # Check args -----------------------------------------------------------------
+
   check_wt_stat_args(x, wt)
+
+  if (!is_number(n)) {
+    stop("`n` must be a number", call. = FALSE)
+  }
+
+  if (n %!in% c(2, 4, 5, 10, 20)) {
+    stop("`n` must be 2, 4, 5, 10, or 20", call. = FALSE)
+  }
 
   if (any(is.na(x)) || any(is.na(wt))) {
     return(NA_integer_)
   }
+
+  # Prep inputs ----------------------------------------------------------------
 
   if (is.logical(x)) {
     x <- as.integer(x)
@@ -51,13 +83,25 @@ wt_median <- function(x, wt) {
 
   share <- wt / sum(wt)
   cum_share <- cumsum(share)
-  k <- match(FALSE, cum_share < 0.5)
 
-  if (cum_share[k] == 0.5) {
-    (x[k] + x[k + 1]) / 2
-  } else {
-    x[k]
+  # Get quantiles --------------------------------------------------------------
+
+  q <- seq_len(n - 1) / n
+
+  output <- vector(mode = "numeric", length = length(q))
+  names(output) <- paste0(round(q * 100), "%")
+
+  for (k in seq_along(q)) {
+    i <- match(TRUE, cum_share >= q[k])
+
+    if (cum_share[i] == q[k]) {
+      output[k] <- (x[i] + x[i + 1]) / 2
+    } else {
+      output[k] <- x[i]
+    }
   }
+
+  output
 }
 
 
