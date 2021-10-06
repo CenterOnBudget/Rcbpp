648f4af524ce07854c3e94c6eeffd72e1b449dea
matt-saenz <55054385+matt-saenz@users.noreply.github.com>
2021-10-06 14:27:35 -0400

    Split

--- R/sp_data.R
+++ R/sp_data.R
@@ -2,28 +2,69 @@
 
 #' Create paths to files in the SharePoint datasets library
 #'
-#' \code{sp_data()} provides a compact way of creating paths to files in the
-#' SharePoint datasets library.
+#' Functions for creating paths to files in the SharePoint datasets library.
+#' These functions are designed to be simple and compact.
 #'
-#' This function creates one or more file paths based on the user's path to
+#' These functions create one or more file paths based on the user's path to
 #' the SharePoint datasets library (accessed via environment variable
 #' \code{spdatapath}) and the directory structure and file naming convention of
-#' each dataset library. This function does not itself verify whether the
-#' resulting files actually exist.
+#' each dataset library. These functions do not verify whether the resulting
+#' files actually exist.
 #'
-#' @param dataset Dataset to build file paths for. One of \code{"acs"},
-#'   \code{"cps_asec"}, or \code{"cps_basic"}.
 #' @param y One or more years.
-#' @param m One or more months (specified numerically). Only applicable if
-#'   \code{dataset} is \code{"cps_basic"}.
+#' @param m One or more months (specified numerically).
 #' @param f Format of data. One of \code{"csv"} or \code{"dta"}.
 #' @return A character vector containing the created file paths.
-#'
+#' @name sp_data
+NULL
+
+
+#' @rdname sp_data
 #' @export
-sp_data <- function(dataset, y, m = NULL, f) {
+sp_acs <- function(y, f) {
+  path <- get_spdatapath()
 
-  # Get SP data path -----------------------------------------------------------
+  check_y(y)
+  check_f(f)
 
+  paste0(path, "ACS/", y, "/", y, "us", ".", f)
+}
+
+
+#' @rdname sp_data
+#' @export
+sp_cps_asec <- function(y, f) {
+  path <- get_spdatapath()
+
+  check_y(y)
+  check_f(f)
+
+  paste0(path, "CPS/mar", y, "/mar", y, ".", f)
+}
+
+
+#' @rdname sp_data
+#' @export
+sp_cps_basic <- function(y, m, f) {
+  path <- get_spdatapath()
+
+  check_y(y)
+
+  if (!is.numeric(m) || !all(m %in% 1:12)) {
+    stop("`m` must be a numeric vector with values ranging from 1 to 12", call. = FALSE)
+  }
+
+  check_f(f)
+
+  df <- expand.grid(m = m, y = y)
+  df$y_sub <- substr(df$y, 3, 4)
+  df$m_abb <- tolower(month.abb)[df$m]
+
+  paste0(path, "CPS-BASIC/", df$y, "/", df$m_abb, df$y_sub, "pub", ".", f)
+}
+
+
+get_spdatapath <- function() {
   path <- Sys.getenv("spdatapath")
 
   if (path == "") {
@@ -33,54 +74,21 @@ sp_data <- function(dataset, y, m = NULL, f) {
     )
   }
 
-  # Check args -----------------------------------------------------------------
+  path
+}
 
-  if (!is_string(dataset)) {
-    stop("`dataset` must be a string", call. = FALSE)
-  }
-
-  if (dataset %!in% c("acs", "cps_asec", "cps_basic")) {
-    stop('`dataset` must be one of "acs", "cps_asec", or "cps_basic"', call. = FALSE)
-  }
-
+check_y <- function(y) {
   if (!is.numeric(y)) {
     stop("`y` must be a numeric vector", call. = FALSE)
   }
+}
 
-  if (dataset == "cps_basic") {
-    if (is.null(m)) {
-      stop('If `dataset` is "cps_basic", you must supply `m`', call. = FALSE)
-    }
-
-    if (!is.numeric(m) || !all(m %in% 1:12)) {
-      stop("`m` must be a numeric vector with values ranging from 1 to 12", call. = FALSE)
-    }
-  } else {
-    if (!is.null(m)) {
-      stop('`m` is only applicable if `dataset` is "cps_basic"', call. = FALSE)
-    }
-  }
-
+check_f <- function(f) {
   if (!is_string(f)) {
     stop("`f` must be a string", call. = FALSE)
   }
 
   if (f %!in% c("csv", "dta")) {
     stop('`f` must be one of "csv" or "dta"', call. = FALSE)
-  }
-
-  # Make file paths ------------------------------------------------------------
-
-  f <- paste0(".", f)
-
-  if (dataset == "cps_asec") {
-    paste0(path, "CPS/mar", y, "/mar", y, f)
-  } else if (dataset == "cps_basic") {
-    df <- expand.grid(m = m, y = y)
-    df$y_sub <- substr(df$y, 3, 4)
-    df$m_abb <- tolower(month.abb)[df$m]
-    paste0(path, "CPS-BASIC/", df$y, "/", df$m_abb, df$y_sub, "pub", f)
-  } else {
-    paste0(path, "ACS/", y, "/", y, "us", f)
   }
 }
