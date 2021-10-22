f495c79c61f7daedc256e3bb1fb565a280db19bd
matt-saenz <55054385+matt-saenz@users.noreply.github.com>
2021-10-22 16:40:10 -0400

    First pass

---
+++ R/zap_stata.R
@@ -0,0 +1,46 @@
+
+
+#' Remove all Stata dataset attributes from a data frame
+#'
+#' \code{zap_stata()} removes all Stata dataset attributes from a data frame
+#' created via \code{haven::read_dta()}. This function is meant to cut down on
+#' repetitive code associated with importing a DTA file when a CSV file is not
+#' readily available. The name of this function is a nod to the
+#' \code{haven::zap_*} functions.
+#'
+#' More specifically, \code{zap_stata()}:
+#'
+#' \itemize{
+#'   \item Removes any value labels and converts any tagged missing values to \code{NA}
+#'   \item Removes any variable labels or formats
+#'   \item Removes the dataset label
+#'   \item Removes any dataset notes
+#' }
+#'
+#' @param df A data frame.
+#' @return A data frame.
+#'
+#' @export
+zap_stata <- function(df) {
+  stopifnot(is.data.frame(df))
+
+  df[] <- lapply(df, zap_stata_vec)
+  attr(df, "label") <- NULL
+  attr(df, "notes") <- NULL
+
+  df
+}
+
+# See https://github.com/tidyverse/haven for details
+
+zap_stata_vec <- function(x) {
+  if (inherits(x, "haven_labelled")) {
+    attr(x, "labels") <- NULL
+    class(x) <- NULL
+  }
+
+  attr(x, "label") <- NULL
+  attr(x, "format.stata") <- NULL
+
+  x
+}
