e07afecaf53b38d31a648b3d21befeaaa23905ff
cz <41382462+c-zippel@users.noreply.github.com>
2026-04-29 15:58:15 -0400

    Rename arg to drop_unused_levels

--- R/statefips.R
+++ R/statefips.R
@@ -6,9 +6,9 @@
 #' @param x A numeric or character vector of state FIPS codes.
 #' @param labels Either `"name"`, to use state names for the factor levels (the
 #'   default), or `"abbrv"`, to use state postal abbreviations.
-#' @param drop_unused Whether to drop unused levels. If `FALSE`, the default,
-#'   the levels of the resulting factor will include every state and state
-#'   equivalent.
+#' @param drop_unused_levels Whether to drop unused levels. If `FALSE`, the
+#'   default, the levels of the resulting factor will include every state and
+#'   state equivalent.
 #'
 #' @returns A factor.
 #'
@@ -20,7 +20,7 @@
 #' fct_statefips(fips)
 #'
 #' # Drop unused factor levels from the output:
-#' fct_statefips(fips, drop_unused = TRUE)
+#' fct_statefips(fips, drop_unused_levels = TRUE)
 #'
 #' # Character vector elements that can't be coerced to integer will be `NA`.
 #' fips_chr <- c("01", "06", "11", "48", "XYZ")
@@ -28,7 +28,11 @@
 #'
 #' @export
 
-fct_statefips <- function(x, labels = c("name", "abbrv"), drop_unused = FALSE) {
+fct_statefips <- function(
+    x,
+    labels = c("name", "abbrv"),
+    drop_unused_levels = FALSE
+) {
   labels <- rlang::arg_match(labels)
 
   if (!(rlang::is_integerish(x) | rlang::is_character(x))) {
@@ -53,7 +57,7 @@ fct_statefips <- function(x, labels = c("name", "abbrv"), drop_unused = FALSE) {
     labels = lbls
   )
 
-  if (drop_unused) {
+  if (drop_unused_levels) {
     f <- droplevels(f)
   }
 
